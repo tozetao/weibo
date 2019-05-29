d7a67fa5a6749530c915b28a310c4da63c9892d9
zetao <tozetao@163.com>
2019-05-29 04:00:18 +0000

    base pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Weibo App') - 新手入门</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+
+@section('title', '主页')
+@section('content')
+    <h1>主页面</h1>
+@stop
